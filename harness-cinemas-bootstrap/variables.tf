3fd58745cdaf3539eafa3d4fc5304c5b51fc92a1
root <cristiano.rosetti@gmail.com>
2022-08-30 05:07:03 +0000

    add vars

--- harness-cinemas-bootstrap/variables.tf
+++ harness-cinemas-bootstrap/variables.tf
@@ -1,7 +1,16 @@
+variable "harness_platform_api_key" {
+  sensitive = true
+}
 variable "harness_platform_account_id" {
   sensitive = true
 }
-variable "harness_platform_api_key" {
+variable "harness_connectors_crizstian_github_token" {
+  sensitive = true
+}
+variable "harness_connectors_crizstian_docker_token" {
+  sensitive = true
+}
+variable "harness_connectors_crizstian_artifactory_token" {
   sensitive = true
 }
 
