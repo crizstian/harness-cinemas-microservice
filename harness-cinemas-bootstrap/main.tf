950e685e2719df03287d932fb75c48f500710332
root <cristiano.rosetti@gmail.com>
2022-08-30 04:19:01 +0000

    add delegates

--- harness-cinemas-bootstrap/main.tf
+++ harness-cinemas-bootstrap/main.tf
@@ -1,6 +1,24 @@
-module "bootstrap_project" {
+module "bootstrap_cinemas_org" {
   source                    = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-project?ref=main"
-  harness_platform_projects = var.harness_platform_projects
+  harness_platform_projects = local.harness_platform_projects
+
+  providers = {
+    harness = harness.provisioner
+  }
+}
+
+module "bootstrap_cinemas_delegates" {
+  source                     = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-delegate?ref=main"
+  harness_platform_delegates = local.harness_platform_delegates
+  harness_platform_api_key   = var.harness_platform_api_key
+}
+
+module "bootstrap_cinemas_connectors" {
+  depends_on = [
+    module.bootstrap_cinemas_org
+  ]
+  source                      = "git::https://github.com/crizstian/harness-terraform-modules.git//harness-connectors?ref=main"
+  harness_platform_connectors = local.harness_platform_connectors
 
   providers = {
     harness = harness.provisioner
@@ -9,6 +27,8 @@ module "bootstrap_project" {
 
 output "details" {
   value = {
-    project = module.bootstrap_project.project
+    organization = module.bootstrap_cinemas_org.organization
+    delegates    = module.bootstrap_cinemas_delegates.delegates
+    project      = module.bootstrap_cinemas_org.project
   }
 }
